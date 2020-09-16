aa36eb477336d52a868757cfb2c5101a6e919988
Jaime Ferrando Huertas <fhjaime96@gmail.com>
2020-09-16 16:49:29 +0200

    initial commit

--- js/ClickarPuntos.js
+++ js/ClickarPuntos.js
@@ -36,4 +36,39 @@ function main() {
         console.log("Se ha liado en la carga de los shaders")
         return;
     }
+
+    gl.clearColor(0.0, 0.0, 0.3, 1.0);
+    gl.clear(gl.COLOR_BUGGET_BIT);
+    var coordenadas = gl.getAttribLocation(gl.program, 'posicion');
+
+    canvas.onmousedown = function (evento) {
+        click(evento, gl, canvas, coordenadas)
+    }
+}
+
+var puntos = []; //Array de puntos
+function click(evento, gl, canvas, coordenadas) {
+    // p.dot = d.vector*[x,y] = g.vector*[x',y'] = g.vector*A*[x,y] Que es A?
+    var x = evento.clientX;
+    var y = evento.clientY;
+    var rect = evento.target.getBoundingclientRect();
+
+    //Conversion de cordenadas
+    x = ((x - rect.left) - canvas.width / 2) * 2 / canvas.width;
+    y = (canvas.height / 2 - (y - rect.top)) * 2 / canvas.height;
+
+    //Guardar los puntos
+    puntos.push(x);
+    puntos.push(y);
+
+    //Borrar el canvas
+    gl.clear(gl.COLOR_BUGGET_BIT);
+
+    //Inserta las coodenadas de los puntos como atributos y los dibuja uno a uno
+
+    for (var i = 0; i < puntos.length; i += 2) {
+        gl.vertexAttrib3f(coordenadas, puntos[i], puntos[i + 1], 0.0);
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
+
 }
