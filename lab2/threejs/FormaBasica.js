8520a07e5744d098ab95adec0a47d356142c23c8
Jaime Ferrando Huertas <fhjaime96@gmail.com>
2020-09-23 16:19:30 +0200

    update

--- lab2/threejs/FormaBasica.js
+++ lab2/threejs/FormaBasica.js
@@ -18,16 +18,13 @@ loadScene();
 render();
 
 function init() {
-
     // Configurar el motor de render y el canvas
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(new THREE.Color(0x0000AA));
     document.getElementById("container").appendChild(renderer.domElement);
-
     // Escena
     scene = new THREE.Scene();
-
     // Camara
     var ar = window.innerWidth / window.innerHeight;
     camera = new THREE.PerspectiveCamera(50, ar, 0.1, 100);
@@ -38,7 +35,6 @@ function init() {
 
 function loadScene() {
     // Construir el grafo de escena
-
     // Materiales
     var material = new THREE.MeshBasicMaterial({
         color: 'yellow',
@@ -47,18 +43,32 @@ function loadScene() {
 
     // Geometrias
     var geocubo = new THREE.BoxGeometry(2, 2, 2);
+    var geoesfera = new THREE.SphereGeometry(1, 30, 30);
 
     // Objetos
     var cubo = new THREE.Mesh(geocubo, material);
+    /// Orden de las transformaciones TRS
+    cubo.rotation.y = Math.PI / 4;
+    cubo.position.x = -1;
+    var esfera = new THREE.Mesh(geoesfera, material);
+    esfera.position.x = 1;
+
+    esferacubo = new THREE.Object3D();
+    esferacubo.position.y = 0.5;
+    esferacubo.rotation.y = angulo;
+
 
     // Organizacion de la escena
-    scene.add(cubo);
+    esferacubo.add(cubo);
+    esferacubo.add(esfera);
+    scene.add(esferacubo);
     scene.add(new THREE.AxisHelper(3));
 }
 
 function update() {
     // Variacion de la escena entre frames
-
+    angulo += Math.PI / 100;
+    esferaCubo.rotation.y = angulo
 }
 
 function render() {
