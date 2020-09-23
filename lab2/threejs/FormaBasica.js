f6f56b2b00b70bcbfe64a4b2473f44d3c0719338
Jaime Ferrando Huertas <fhjaime96@gmail.com>
2020-09-23 15:56:32 +0200

    update

--- lab2/threejs/FormaBasica.js
+++ lab2/threejs/FormaBasica.js
@@ -20,5 +20,45 @@ function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(new THREE.Color(0x0000AA));
+    document.getElementById("container").appendChild(renderer.domElement);
+
+    // Escena
+    scene = new THREE.scene()
+
+    //Camara
+    var ar = window.innerWidth / window.innerHeight;
+    camera = new THREE.PerspecctiveCamera(50, ar, 0.1, 100);
+    scene.add(camera);
+    camera.position.set(0.5, 2, 5);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+}
+
+function loadScene() {
+    // Construir el grafo de la escena
+
+    // Materiales
+    var material = new THREE.MeshBasicMaterial({
+        color: "yellow",
+        wireframe: true
+    });
+    //Geometrias
+    var geocubo = new THREE.BoxGeometry(2, 2, 2);
+    //Objectos
+    var cubo = new THREE.Mesh(geocubo, material);
+    // Organizacion de la escena
+    scene.add(cubo);
+
+
+}
+
+function update() {
+    // variacion de la escena entre frames
+}
+
+function render() {
+    // Construir el frame y mostrarlo
+    requestAnimationFrame(render);
+    update();
+    renderer.render(scene, camera);
 
 }
