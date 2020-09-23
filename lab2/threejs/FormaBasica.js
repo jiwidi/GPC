fc9b28bc1f22e756653366f91dded10aa1889d15
Jaime Ferrando Huertas <fhjaime96@gmail.com>
2020-09-23 16:23:34 +0200

    update

--- lab2/threejs/FormaBasica.js
+++ lab2/threejs/FormaBasica.js
@@ -18,23 +18,27 @@ loadScene();
 render();
 
 function init() {
+
     // Configurar el motor de render y el canvas
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(new THREE.Color(0x0000AA));
     document.getElementById("container").appendChild(renderer.domElement);
+
     // Escena
     scene = new THREE.Scene();
+
     // Camara
     var ar = window.innerWidth / window.innerHeight;
     camera = new THREE.PerspectiveCamera(50, ar, 0.1, 100);
     scene.add(camera);
-    camera.position.set(0.5, 2, 5);
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    camera.position.set(0.5, 3, 9);
+    camera.lookAt(new THREE.Vector3(0, 2, 0));
 }
 
 function loadScene() {
     // Construir el grafo de escena
+
     // Materiales
     var material = new THREE.MeshBasicMaterial({
         color: 'yellow',
@@ -46,6 +50,7 @@ function loadScene() {
     var geoesfera = new THREE.SphereGeometry(1, 30, 30);
 
     // Objetos
+
     var cubo = new THREE.Mesh(geocubo, material);
     /// Orden de las transformaciones TRS
     cubo.rotation.y = Math.PI / 4;
@@ -53,9 +58,18 @@ function loadScene() {
     var esfera = new THREE.Mesh(geoesfera, material);
     esfera.position.x = 1;
 
+    /// Objeto contenedor
     esferacubo = new THREE.Object3D();
     esferacubo.position.y = 0.5;
     esferacubo.rotation.y = angulo;
+
+    /// Modelo externo
+    var loader = new THREE.ObjectLoader();
+    loader.load('models/soldado/soldado.json',
+        function (obj) {
+            obj.position.set(0, 1, 0);
+            cubo.add(obj);
+        });
 
 
     // Organizacion de la escena
@@ -68,7 +82,7 @@ function loadScene() {
 function update() {
     // Variacion de la escena entre frames
     angulo += Math.PI / 100;
-    esferaCubo.rotation.y = angulo
+    esferacubo.rotation.y = angulo;
 }
 
 function render() {
